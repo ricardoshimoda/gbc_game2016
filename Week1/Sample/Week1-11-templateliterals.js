9d249131f3438adfdbc7dc07a20bae683ade136e
rshimoda <rshimoda@gmail.com>
2018-09-05 18:01:48 -0400

    First Week Material

---
+++ Week1/Sample/Week1-11-templateliterals.js
@@ -0,0 +1,14 @@
+﻿"user strict"
+
+let name = 'John';
+
+function makeUppercase(word)
+{
+    return word.toUpperCase();
+}
+
+let template =
+    `<h1>${makeUppercase('Hello')}, ${name} </h1>
+<p> this is a simple template in Javascript</p>`;
+
+document.getElementById("template").innerHTML = template;
---
+++ Week1/Sample/Week1-9-classes.js
@@ -0,0 +1,37 @@
+﻿"use strict"
+class User {
+    constructor(username, email, password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    register() {
+        console.log(this.username + ' is now registered');
+    }
+
+    static countUser() {
+        console.log('there are 2 users');
+    }
+}
+
+
+class Member extends User {
+    constructor(username, email, password, member, memberPackage) {
+        super(username, email, password);
+        this.package = memberPackage
+    }
+
+    getPackage() {
+        console.log(this.username + " is subscribed to " + this.memberPackage);
+    }
+
+}
+
+let bob = new User('bob', 'bob@hotmail.com', '12345');
+bob.register();
+User.countUser();
+
+let mike = new Member('mike', 'bob@hotmail.com', '12345', 'standard');
+mike.register();
+mike.getPackage();
